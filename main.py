e64518a19925a3815f40734b87ba4e50131c882e
thiago nieva <nievathiago24@gmail.com>
2025-12-10 21:38:02 -0300

    ruta usuarios y cursos agregada

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from fastapi import FastAPI
-app =FastAPI()
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app =FastAPI()    
+
 @app.get("/health")
 def obtener_salud_sitio():
-    return "ok"
+    return "sapo"
+
+@app.get("/usuarios")
+def obtener_usuarios():
+    collection = db.collection("usuarios").stream()
+    return [c.to_dict() for c in collection]
+
+
+@app.get("/cursos")
+def obtener_cursos():
+    collection = db.collection("cursos").stream()
+    return [c.to_dict() for c in collection]
